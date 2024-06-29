fe111fc39e087a5f20f86412663c0d1ede0b23d4
Otacilio Maia <otacilio.n97@gmail.com>
2024-06-29 14:44:12 -0300

    Update firebase.js

--- obelisco/src/firebase.js
+++ obelisco/src/firebase.js
@@ -6,14 +6,7 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC39oJyAeH6gSAf-PreJEs0wDpbXLtnQpU",
-  authDomain: "excellent-hue-425111-b0.firebaseapp.com",
-  databaseURL: "https://excellent-hue-425111-b0-default-rtdb.firebaseio.com",
-  projectId: "excellent-hue-425111-b0",
-  storageBucket: "excellent-hue-425111-b0.appspot.com",
-  messagingSenderId: "665758919696",
-  appId: "1:665758919696:web:ddd2de556aa528e120ae95",
-  measurementId: "G-5H6VMTJGTH",
+  apiKey: "yourdbdata",
 };
 
 // Initialize Firebase
